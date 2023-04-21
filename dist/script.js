860059a697d7d262c4400b69020950157763db77
NasonovVA <12345Vados@mail.ru>
2023-04-21 21:26:29 +0300

    Vados

---
+++ dist/script.js
@@ -0,0 +1,14 @@
+console.log("helo world");
+console.log("10/0");
+//alert('vj;yj')
+var message = 1;
+message = 2 + 3 + 2;
+var daysinweek = message;
+console.log(daysinweek);
+console.log('message=message+2;');
+var admin = JON;
+var name = JON;
+alert;
+JON;
+var BIRTHDAY = ;
+var age = ;
---
+++ script.ts
@@ -0,0 +1,14 @@
+console.log("helo world")
+console.log("10/0")
+//alert('vj;yj')
+let message=1
+message=2+3+2
+const daysinweek=message
+console.log(daysinweek)
+console.log('message=message+2;')
+let admin = JON
+let name = JON
+alert JON
+const BIRTHDAY =
+const age =
+
