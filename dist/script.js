0380d8d6c2ec0a4b3090bdb0f0318e77cc9c69ce
NasonovVA <12345Vados@mail.ru>
2023-04-28 21:26:29 +0300

    n?kn

--- dist/script.js
+++ dist/script.js
@@ -16,3 +16,35 @@ var s2 = 4;
 console.log('4>3,4>3');
 console;
 //practice_1_1
+//allert(km*0.621371)
+//allert('1+1=${1+1}
+//1-1=${1-1}')
+var hours = +prompt('часы');
+var minutes = +prompt('минуты');
+var timeInMinutes = hours * 60 + minutes;
+var timeInMinutesToNextDay = 24 * 60 - timeInMinutes;
+var timeInMinutesDay = 12;
+var hoursToNextDay = 15;
+var number = 278;
+console.log(278 - (278 % 10) / 10 % 10);
+if (2015 == 2015)
+    console.log('вы правы');
+var secret = "0123";
+var pass = prompt(' пароль');
+if (secret == pass) {
+    console.log('вы вошли');
+}
+else if (secret == pass) {
+    console.log('вы вошли как админ');
+}
+else {
+    console.log('вы не вошли');
+}
+//if ( условие) {
+// блок кода , которые выпонится если условия истинны
+//}
+var age1 = 20;
+var accessAlowed;
+if (age > 18) {
+    var age2 = 2;
+}
--- script.ts
+++ script.ts
@@ -22,3 +22,39 @@ console
 
 //practice_1_1
 
+
+//allert(km*0.621371)
+//allert('1+1=${1+1}
+//1-1=${1-1}')
+
+const hours = +(prompt('часы')as string)
+const minutes = +(prompt('минуты') as string)
+const timeInMinutes = hours*60 + minutes
+const timeInMinutesToNextDay = 24 * 60 - timeInMinutes
+const timeInMinutesDay = 12
+const hoursToNextDay = 15
+
+
+
+const number = 278
+console.log(278 - (278%10)/10%10)
+
+if (2015 == 2015) console.log('вы правы')
+const secret = "0123"
+const pass = prompt(' пароль')
+if (secret == pass) {
+    console.log('вы вошли')
+} else if (secret == pass){
+    console.log('вы вошли как админ')
+} else { 
+    console.log('вы не вошли')
+}
+
+//if ( условие) {
+// блок кода , которые выпонится если условия истинны
+//}
+
+const age1 = 20
+let accessAlowed
+if (age >18) {let age2 = 2 }
+
